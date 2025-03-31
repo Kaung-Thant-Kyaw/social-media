817a576e3fc1ac8c868aed8febce4c58af545d6c
Kaung Thant Kyaw <kaungthantkyaw112497@gmail.com>
2025-03-31 15:50:48 +0630

    database migration

---
+++ database/migrations/2025_03_27_063209_create_social_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('social_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('provider');
+            $table->string('prover_id');
+            $table->text('token');
+            $table->text('refresh_token')->nullable();
+            $table->timestamp('expired_at')->nullable();
+            $table->timestamps();
+
+            $table->unique(['user_id', 'provider']);
+            $table->unique(['provider', 'provider_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('social_accounts');
+    }
+};
